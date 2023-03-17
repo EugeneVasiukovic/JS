65b464b50f7b0302caa5627416513d06597788cc
EugeneVasiukovic <askarishwaine@gmail.com>
2023-03-17 22:54:47 +0300

    hw1

--- HW 1.js
+++ HW 1.js
@@ -26,7 +26,7 @@
 //19. Присвоить переменной item_6 значение 15
 //20. Присвоить переменной item_6_type тип переменной item_6
 var item_6 = 15;
-var item_6_type = 15;
+var item_6_type = typeof item_6;
 // 21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
 console.log('item_6 ==', typeof item_6)
 console.log('item_6_type ==', typeof item_6_type)
@@ -34,7 +34,7 @@ console.log('item_6_type ==', typeof item_6_type)
 var item_7 = String(item_6);
 //23. Создать переменную item_7_type
 //24. Присвоить переменной item_7_type тип переменной item_7
-var item_7_type = item_7;
+var item_7_type = typeof item_7;
 //25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
 console.log('item_7 ==', typeof item_7);
 console.log('item_7_type ==', typeof item_7_type);
